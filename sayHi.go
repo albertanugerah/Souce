93200364580ce7940c04c7db09a9b87bb72565a1
albertanugerah <albertanugerah@hotmail.com>
2021-01-21 19:33:27 +0700

    buat comment

--- sayHi.go
+++ sayHi.go
@@ -3,3 +3,16 @@ package go_module
 func SayHi() string {
 	return "Hello"
 }
+
+// git mod init github.com/albertanugerah/go-module
+// git init
+// git add go.mod
+// git add sayHi.go
+// git status
+// git commit -m ""
+// git remote add origin https://github.com/albertanugerah/go-module.git
+// git push origin master
+
+// membuat version
+// git tag v1.0.0
+// git push origin v1.0.0
